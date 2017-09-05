f5e9fd2f6d52f4b73d757c3e363bb00a3f3e89c6
Josh Buchea <verctrl@joshbuchea.com>
2017-09-05 13:43:00 -0700

    Log target page console to console (useful for debugging)

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
+  // log console to console
+  page.on('console', (...args) => {
+    console.log('PAGE LOG: ', ...args);
+  });
   await page.goto('https://example.com');
   await page.screenshot({ path: 'example.png' });
 
