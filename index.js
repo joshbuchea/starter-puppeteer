5b852ea31e644491ed0b92a333655ee9ad341996
Josh Buchea <verctrl@joshbuchea.com>
2017-09-05 13:42:16 -0700

    Launch with headless mode disabled by default (useful for debugging)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({ path: 'example.png' });
