a9a600aef5165d4dfbb458bca83e7718d6b845b6
Josh Buchea <verctrl@joshbuchea.com>
2017-09-05 13:45:46 -0700

    Add a couple screenshot examples, update existing screenshot example

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const puppeteer = require('puppeteer');
     console.log('PAGE LOG: ', ...args);
   });
   await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+
+  // screenshot: viewport
+  await page.screenshot({ path: 'example-viewport.png' });
+
+  // screenshot: full page
+  await page.screenshot({ path: 'example-full.png', fullPage: true });
+
+  // screenshot: page coordinates
+  await page.screenshot({
+    path: 'example-coords.png',
+    clip: { x: 215, y: 0, width: 390, height: 50 },
+  });
 
   browser.close();
 })();
